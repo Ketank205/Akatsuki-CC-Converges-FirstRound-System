a53b4d1c41c0864a0783929372dfe7708b6f79cd
Bhavesh Rajput <159777947+bhavesh11230@users.noreply.github.com>
2025-02-22 13:28:51 +0530

    Update verifyController.js

--- controllers/verifyController.js
+++ controllers/verifyController.js
@@ -4,15 +4,9 @@ import { convertBinary, getUniqueNode, getAnimeCharacter } from "../utils/utilit
 const verifyConversion = async (req, res) => {
     try {
         const { teamId, binaryNumber, numberSystem, convertedValue } = req.body;
-        
-        let team = await Team.findOne({ teamId, binaryNumber, numberSystem });
+        // console.log(teamId, binaryNumber, numberSystem, convertedValue);
 
-        // Check if the team already has an assigned computer character
-        if (team && team.animeCharacter && team.animeCharacter.name) {
-            return res.status(400).json({
-                status: "You have already been assigned a computer character. No further submissions allowed."
-            });
-        }
+        let team = await Team.findOne({ teamId, binaryNumber, numberSystem });
 
         let attempts = team ? team.attempts : 0;
         let assignedCharacter = null;
@@ -91,6 +85,7 @@ const verifyConversion = async (req, res) => {
             pointsRecieved
         });
 
+        // console.log("Team data updated successfully!");
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Server error due to Incorrect Inputs!" });
@@ -98,103 +93,6 @@ const verifyConversion = async (req, res) => {
 };
 
 export default verifyConversion;
-
-
-// import Team from "../models/Team.js";
-// import { convertBinary, getUniqueNode, getAnimeCharacter } from "../utils/utility.js";
-
-// const verifyConversion = async (req, res) => {
-//     try {
-//         const { teamId, binaryNumber, numberSystem, convertedValue } = req.body;
-//         // console.log(teamId, binaryNumber, numberSystem, convertedValue);
-
-//         let team = await Team.findOne({ teamId, binaryNumber, numberSystem });
-
-//         let attempts = team ? team.attempts : 0;
-//         let assignedCharacter = null;
-//         let imageUrl = null;
-//         let assignedNode = null;
-//         let pointsRecieved = 0;
-//         let status = "Correct Conversion";
-
-//         const expectedValue = convertBinary(binaryNumber, numberSystem);
-
-//         if (expectedValue !== convertedValue) {
-//             attempts++;
-//             if (attempts < 3) {
-//                 status = "Incorrect Conversion. Try Again!";
-//                 if (team) {
-//                     team.attempts = attempts;
-//                     await team.save();
-//                 } else {
-//                     team = new Team({
-//                         teamId,
-//                         binaryNumber,
-//                         convertedValue: null, // No correct conversion yet
-//                         numberSystem,
-//                         animeCharacter: null,
-//                         assignedNode: null,
-//                         pointsRecieved: 0,
-//                         attempts
-//                     });
-//                     await team.save();
-//                 }
-//                 return res.status(400).json({ status, attemptsLeft: 3 - attempts });
-//             } else {
-//                 status = "Unsuccessful Conversion after 3 attempts";
-//                 pointsRecieved = 25;
-//             }
-//         } else {
-//             let arry = getAnimeCharacter();
-//             assignedCharacter = arry[0];
-//             imageUrl = arry[1];
-//             pointsRecieved = 50;
-//         }
-//         assignedNode = getUniqueNode();
-
-//         if (!team) {
-//             team = new Team({
-//                 teamId,
-//                 binaryNumber,
-//                 convertedValue,
-//                 numberSystem,
-//                 animeCharacter: {
-//                     name: assignedCharacter,
-//                     imageUrl: imageUrl
-//                 },
-//                 assignedNode,
-//                 pointsRecieved,
-//                 attempts
-//             });
-//         } else {
-//             team.convertedValue = convertedValue;
-//             team.animeCharacter = {
-//                 name: assignedCharacter,
-//                 imageUrl: imageUrl
-//             };
-//             team.assignedNode = assignedNode;
-//             team.pointsRecieved = pointsRecieved;
-//             team.attempts = attempts;
-//         }
-
-//         await team.save();
-
-//         res.json({
-//             status,
-//             assignedCharacter: team.animeCharacter?.name || null,
-//             imageUrl: team.animeCharacter?.imageUrl || null,
-//             assignedNode,
-//             pointsRecieved
-//         });
-
-//         // console.log("Team data updated successfully!");
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: "Server error due to Incorrect Inputs!" });
-//     }
-// };
-
-// export default verifyConversion;
 // import Team from "../models/Team.js";
 // import { convertBinary, getUniqueNode, getAnimeCharacter } from "../utils/utility.js";
 

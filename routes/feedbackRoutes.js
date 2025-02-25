1596a115fcb640c07bd602a985e79f24f58ed503
Bhavesh Rajput <159777947+bhavesh11230@users.noreply.github.com>
2025-02-25 11:29:04 +0530

    Update feedbackRoutes.js

--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -1,8 +1,19 @@
+// import express from "express";
+// import verifyConversion from "../controllers/verifyController.js";
+
+// const router = express.Router();
+
+// router.post("/verify", verifyConversion);
+
+// export default router;
+
 import express from "express";
 import verifyConversion from "../controllers/verifyController.js";
+import addTeam from "../controllers/addTeam.js";
 
 const router = express.Router();
 
 router.post("/verify", verifyConversion);
+router.post("/add", addTeam);
 
 export default router;
